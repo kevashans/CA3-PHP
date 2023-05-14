2d4b0f50156a79a729dfa72660f3883a25b5e3ef
Matthew-Riddell <d00245674@student.dkit.ie>
2023-05-14 13:48:10 +0100

    Create Forum Styling

--- resources/views/forums/create.blade.php
+++ resources/views/forums/create.blade.php
@@ -1,57 +1,99 @@
+
+
 @extends('layouts.app')
 
 @section('content')
-<div class="w-4/5 m-auto text-left">
-    <div class="py-15">
-        <h1 class="text-6xl">
-            Create Forum
-        </h1>
+<div class="sm:container sm:mx-auto sm:max-w-lg sm:mt-10">
+    <div class="w-full">
+        <section class="flex flex-col break-words bg-white sm:border-1 sm:rounded-md sm:shadow-sm sm:shadow-lg">
+
+            <header class="font-semibold bg-gray-200 text-gray-700 py-5 px-6 sm:py-6 sm:px-8 sm:rounded-t-md">
+                <h1 class="text-3xl">
+                    Create Forum
+                </h1>
+            </header>
+
+            <form class="w-full px-6 space-y-6 sm:px-10 sm:space-y-8"
+                action="/forums"
+                method="POST"
+                enctype="multipart/form-data">
+                @csrf
+
+                <div class="flex flex-wrap">
+                    <label for="topic_name" class="block text-gray-700 text-sm font-bold mb-2 sm:mb-4">
+                        {{ __('Title') }}:
+                    </label>
+
+                    <input id="topic_name" type="text" class="form-input w-full @error('topic_name') border-red-500 @enderror"
+                        name="topic_name" value="{{ old('topic_name') }}" required autofocus>
+
+                    @error('topic_name')
+                        <p class="text-red-500 text-xs italic mt-4">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <div class="flex flex-wrap">
+                    <label for="topic_description" class="block text-gray-700 text-sm font-bold mb-2 sm:mb-4">
+                        {{ __('Description') }}:
+                    </label>
+
+                    <textarea id="topic_description" class="form-input w-full @error('topic_description') border-red-500 @enderror"
+                        name="topic_description" rows="5" required>{{ old('topic_description') }}</textarea>
+
+                    @error('topic_description')
+                        <p class="text-red-500 text-xs italic mt-4">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <div class="flex flex-wrap">
+                    <label for="image" class="block text-gray-700 text-sm font-bold mb-2 sm:mb-4">
+                        {{ __('Image') }}:
+                    </label>
+
+                    <label for="image" class="w-full bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center justify-center cursor-pointer border border-gray-300">
+                    <svg class="w-4 h-4 fill-current mr-2" viewBox="0 0 20 20">
+                    <path d="M17.16 5.63l-4.42-4.42A2 2 0 0 0 12.17 1H7.83a2 2 0 0 0-1.57.75L2.84 5.63A2 2 0 0 0 2 7.09v8.82a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.09a2 2 0 0 0-.84-1.46zM10 14a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm3-7h-2V5h-1v2H7v1h2v2h1v-2h2z"/>
+                    </svg>
+                    <span class="text-base leading-normal">{{ __('Select a file') }}</span>
+                    <input id="image" type="file" name="image" class="hidden">
+                    </label>
+
+                    @error('image')
+                        <p class="text-red-500 text-xs italic mt-4">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <div class="flex flex-wrap">
+                    <label for="tags" class="block text-gray-700 text-sm font-bold mb-2 sm:mb-4">
+                        {{ __('Tags') }}:
+                    </label>
+
+                    @foreach ($tags as $tag)
+                        <span class="m-3">
+                            <input id="{{ $tag->name }}" class="rounded" type="checkbox" name="tags[]" value="{{ $tag->name }}">
+                            <label class="ml-2" for="{{ $tag->name }}">{{ $tag->name }}</label>
+                        </span>
+                    @endforeach    
+                </div>
+
+                <div class="flex flex-wrap">
+                        <button type="submit"
+                            class="w-full select-none font-bold whitespace-no-wrap p-3 rounded-lg text-base leading-normal no-underline text-gray-100 colored_button hover:colored_button sm:py-4">
+                            {{ __('Create Forum') }}
+                        </button>
+                </div>               
+            </form>
+        </section>
     </div>
 </div>
 
-<div class="w-4/5 m-auto pt-20">
-    <form 
-        action="/forums"
-        method="POST"
-        enctype="multipart/form-data">
-        @csrf
-
-        <input 
-            type="text"
-            name="topic_name"
-            placeholder="Title..."
-            class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
-
-        <textarea 
-            name="topic_description"
-            placeholder="Description..."
-            class="py-20 bg-transparent block border-b-2 w-full h-60 text-xl outline-none"></textarea>
-
-            <div class="bg-grey-lighter pt-15">
-                <label class="w-44 flex flex-col items-center px-2 py-3 bg-white-rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer">
-                    <span class="mt-2 text-base leading-normal">
-                        Select a file
-                    </span>
-                    <input 
-                        type="file"
-                        name="image"
-                        class="hidden">
-                </label>
-            </div>
-
-        <button    
-            type="submit"
-            class="uppercase mt-15 colored_button text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
-            Create Forum
-        </button>
-        @foreach ($tags as $tag)
-        <span class="m-3">
-            
-            <input class="rounded" type="checkbox" name="tags[]" value= {{ $tag->name }} id={{ $tag->name }}>
-            <label class="ml-2" for={{ $tag->name }}>{{ $tag->name }}</label>
-        </span>
-        @endforeach
-    </form>
-</div>
-
 @endsection
+
+
+
